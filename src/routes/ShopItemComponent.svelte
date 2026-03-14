eacf357234a3f065653be6b474801f56e4a60b92
Hanya You <youh@catlin.edu>
2026-03-14 09:05:49 -0700

    still trying to fix images

--- src/routes/ShopItemComponent.svelte
+++ src/routes/ShopItemComponent.svelte
@@ -21,15 +21,15 @@
     <div class="w-full p-2 text-blue-800 px-2 py-1">
         <!-- svelte-ignore a11y_click_events_have_key_events -->
         <!-- svelte-ignore a11y_no_static_element_interactions -->
-        <div onclick={buy} class="flex items-stretch border border-2 border-blue-800 {!faded ? "-translate-[2px] shadow-[2px_2px_0px_0px_var(--color-blue-800)] bg-rose-200" : "bg-rose-200/50"} {isPressed ? "shadow-[inset_1px_1px_0px_0px_var(--color-blue-800)] translate-0 p-[5px_1px_3px_1px]" : "py-[4px]"}">
-            <div class="w-28 mr-2 bg-contain bg-center bg-no-repeat px-2 border-r-2 border-blue-800">
+        <div onclick={buy} class="grid grid-cols-5 items-stretch border border-2 border-blue-800 {!faded ? "-translate-[2px] shadow-[2px_2px_0px_0px_var(--color-blue-800)] bg-rose-200" : "bg-rose-200/50"} {isPressed ? "shadow-[inset_1px_1px_0px_0px_var(--color-blue-800)] translate-0 p-[5px_1px_3px_1px]" : "py-[4px]"}">
+            <div class="col-span-1 mr-2 bg-contain px-2 border-r-2 border-blue-800">
                 {#if !hidden}
                     <img src={item.icon} class="size-full object-center object-contain hover:scale-110 transition duration-50">
                 {:else}
                     <div class="bg-blue-800 size-full hover:scale-110 transition duration-50" style="mask-image: url({item.icon}); mask-size: contain; mask-repeat: no-repeat; mask-position: center"></div>
                 {/if}
             </div>
-            <div>
+            <div class="col-span-4">
                 <h1 class="text-4xl {!hidden ? "font-[Star-Crush]" : 'font-[RetroByte]'}">{!hidden ? item.name : '???'}</h1>
                 <span class="block">{!hidden ? item.description : '???'}</span>
                 <span class="text-md">
